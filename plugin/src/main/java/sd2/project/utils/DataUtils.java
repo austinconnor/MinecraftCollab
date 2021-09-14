ff895f759a3298b2ce92a6636704c07519ac41e9
Andrew <aamado@knights.ucf.edu>
2021-09-14 18:58:04 -0400

    Added event property in JSON data

--- plugin/src/main/java/sd2/project/utils/DataUtils.java
+++ plugin/src/main/java/sd2/project/utils/DataUtils.java
@@ -19,6 +19,7 @@ import com.mongodb.client.MongoDatabase;
 
 import org.bson.Document;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 
 public class DataUtils 
 {
@@ -36,7 +37,7 @@ public class DataUtils
 
     // This method provides us a useable JSON object that we can tweak to our needs.
     // We take a player as a parameter since we would like to get their location.
-    public JsonObject packageData(Player player)
+    public JsonObject packageData(Player player, Event e)
     {
 
 
@@ -53,6 +54,7 @@ public class DataUtils
         // Adding necessary data to our object.
         json.addProperty("player", player.getName());
         json.add("location", gson.toJsonTree(locationData));
+        json.addProperty("event", e.getEventName());
         
         // An example of an output: {"player":"Stoworm","location":{"worldName":"world","x":223,"y":72,"z":92,"worldTime":1092}}
         return json;
