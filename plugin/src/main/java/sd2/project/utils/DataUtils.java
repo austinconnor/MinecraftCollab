b4fd5479e3cfc6f4486c8a039cb65f49ad9b3f93
Andrew <aamado@knights.ucf.edu>
2021-09-14 18:11:57 -0400

    For DB update

--- plugin/src/main/java/sd2/project/utils/DataUtils.java
+++ plugin/src/main/java/sd2/project/utils/DataUtils.java
@@ -1,15 +1,33 @@
 package sd2.project.utils;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
+import org.bson.Document;
 import org.bukkit.entity.Player;
 
 public class DataUtils 
 {
     
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    private FileWriter outputFile;
+    public String outputFileName = "output.json";
+	private String mongoURI = "placeholder";
 
     public int hashCode(Player player)
     {
@@ -20,6 +38,7 @@ public class DataUtils
     // We take a player as a parameter since we would like to get their location.
     public JsonObject packageData(Player player)
     {
+
 
         JsonObject json = new JsonObject();
         JsonObject locationData = new JsonObject();
@@ -36,8 +55,87 @@ public class DataUtils
         json.add("location", gson.toJsonTree(locationData));
         
         // An example of an output: {"player":"Stoworm","location":{"worldName":"world","x":223,"y":72,"z":92,"worldTime":1092}}
-
         return json;
     }
 
+    // Upon successfully writing to the local file, return true.
+    public boolean writeToFile(JsonObject data)
+    {
+        try
+        {
+            // Declare and write to the output file.
+            outputFile = new FileWriter(outputFileName, true);
+            outputFile.append(data.toString() + "\n");
+            outputFile.close();
+        }
+        catch(IOException exception)
+        {
+            exception.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    // Converts output.json into an ArrayList
+    public List<Document> parseFileToList(String fileName)
+    {
+        ArrayList<Document> data = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName)))
+        {
+            String line;
+
+            while ((line = br.readLine()) != null)
+            {
+                Document document = Document.parse(line);
+                data.add(document);
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+
+    public boolean clearFile(String fileName)
+    {
+        try
+        {
+            PrintWriter writer = new PrintWriter(fileName);
+            writer.print("");
+            writer.close();
+        }
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+
+        return true;
+    }
+
+    // Uploads current data to the database.
+    public int writeToDB()
+    {
+        MongoClient mongoClient = new MongoClient(new MongoClientURI(mongoURI));
+        MongoDatabase database = mongoClient.getDatabase("Data");
+        MongoCollection<Document> collection = database.getCollection("worldData");
+
+        List<Document> dataList = parseFileToList(outputFileName);
+
+        // Get the file output.json
+        try 
+        {
+            System.out.println("Size of list" + dataList.size());
+            collection.insertMany(dataList);
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+        mongoClient.close();
+        return dataList.size();
+    }
 }
