8cab3269dca01a3dae137a028f8be00af9eced7f
Chloe Geller <chloegeller@knigths.ucf.edu>
2021-11-02 16:19:25 -0400

    added new plots, and more events

--- src/js/2D.js
+++ src/js/2D.js
@@ -1,58 +1,253 @@
-var data = [];
+function createPlot(folder_path, file_event, event_name, div_name) {
+    var data = [];
 
-function createTrace(filename, player, mode, type){
-    d3.csv(filename, function(err, rows) {
-    
-        function unpack(rows, key) {
-            return rows.map(function(row)
-            { return row[key]; });
-        }
+    function createTrace(data, filename, player, mode, type){
+
+        d3.csv(filename, function(err, rows) {
         
-        r = Math.floor(Math.random() * 256).toString()
-        g = Math.floor(Math.random() * 256).toString()
-        b = Math.floor(Math.random() * 256).toString()
-        random_color = 'rgb(' + r + ',' + g + ',' + b +')';
-
-        var trace = {
-        x: unpack(rows, 'x'),
-        y: unpack(rows, 'z'),
-        mode: mode,
-        name: player,
-        marker: {
-            color: random_color,
-            size: 2,
-            symbol: 'circle',
-            line: {
+            function unpack(rows, key) {
+                return rows.map(function(row)
+                { return row[key]; });
+            }
+    
+            r = Math.floor(Math.random() * 256).toString()
+            g = Math.floor(Math.random() * 256).toString()
+            b = Math.floor(Math.random() * 256).toString()
+            random_color = 'rgb(' + r + ',' + g + ',' + b +')';
+            var trace = {
+            x: unpack(rows, 'x'),
+            y: unpack(rows, 'z'),
+            mode: mode,
+            name: player,
+            marker: {
                 color: random_color,
-                width: 0
-            }},
-        line: {
-            color: random_color,
-            width: 2
-        },
-        type: type
-        };
+                size: 3,
+                symbol: 'circle',
+            },
+            type: type
+            };
+            data.push(trace);
+        });
+    }
 
-        data.push(trace)
-        console.log(trace)
-    });
+    for(var i = 0; i < 10; i++){
+        createTrace(data, folder_path + i.toString() + file_event + '.csv', 'Player ' + i.toString(), 'markers', 'scatter3d')
+    }
+    
+    var layout = {
+        title: event_name,
+        autosize: true,
+        width: 1500,
+        height: 1500,
+        margin: {
+            l: 0,
+            r: 0,
+            b: 0,
+            t: 65
+        }
+    };
+    
+    setTimeout(function(){ console.log(data); Plotly.newPlot(div_name, data, layout, {responsive: true, displaylogo: false}); }, 5000);
 }
 
-for(var i = 10; i < 25; i++){
-    createTrace('./data/'+ i.toString() + '_move.csv', i.toString(), 'markers', 'scatter')
-}
+createPlot('./data/overworld/', '_move', 'PlayerMoveEvent', 'Overworld_Move_2DPoints');
 
-var layout = {
-title: '2D Movement Plot',
-autosize: true,
-width: 1500,
-height: 1500,
-margin: {
-    l: 0,
-    r: 0,
-    b: 0,
-    t: 65
-}
-};
 
-setTimeout(function(){ console.log(data); Plotly.newPlot('my2DMap', data, layout, {responsive: true, displaylogo: false}); }, 5000);
+// // from http://bl.ocks.org/mbostock/4349187
+// // Sample from a normal distribution with mean 0, stddev 1.
+
+// function normal() {
+//     var x = 0,
+//         y = 0,
+//         rds, c;
+//     do {
+//         x = Math.random() * 2 - 1;
+//         y = Math.random() * 2 - 1;
+//         rds = x * x + y * y;
+//     } while (rds == 0 || rds > 1);
+//     c = Math.sqrt(-2 * Math.log(rds) / rds); // Box-Muller transform
+//     return x * c; // throw away extra sample y * c
+// }
+
+// var N = 2000,
+//   a = -1,
+//   b = 1.2;
+
+// var step = (b - a) / (N - 1);
+// var t = new Array(N), x = new Array(N), y = new Array(N);
+
+// for(var i = 0; i < N; i++){
+//   t[i] = a + step * i;
+//   x[i] = (Math.pow(t[i], 3)) + (0.3 * normal() );
+//   y[i] = (Math.pow(t[i], 6)) + (0.3 * normal() );
+// }
+
+// var trace1 = {
+//   x: x,
+//   y: y,
+//   mode: 'markers',
+//   name: 'points',
+//   marker: {
+//     color: 'rgb(102,0,0)',
+//     size: 2,
+//     opacity: 0.4
+//   },
+//   type: 'scatter'
+// };
+// var trace2 = {
+//   x: x,
+//   y: y,
+//   name: 'density',
+//   ncontours: 20,
+//   colorscale: 'Hot',
+//   reversescale: true,
+//   showscale: false,
+//   type: 'histogram2dcontour'
+// };
+// var trace3 = {
+//   x: x,
+//   name: 'x density',
+//   marker: {color: 'rgb(102,0,0)'},
+//   yaxis: 'y2',
+//   type: 'histogram'
+// };
+// var trace4 = {
+//   y: y,
+//   name: 'y density',
+//   marker: {color: 'rgb(102,0,0)'},
+//   xaxis: 'x2',
+//   type: 'histogram'
+// };
+
+// // var data = [trace1, trace2, trace3, trace4];
+// var layout = {
+//   showlegend: true,
+//   autosize: false,
+//   width: 1500,
+//   height: 1500,
+//   margin: {t: 50},
+//   hovermode: 'closest',
+//   bargap: 0,
+//   xaxis: {
+//     domain: [0, 0.85],
+//     showgrid: false,
+//     zeroline: false
+//   },
+//   yaxis: {
+//     domain: [0, 0.85],
+//     showgrid: false,
+//     zeroline: false
+//   },
+//   xaxis2: {
+//     domain: [0.85, 1],
+//     showgrid: false,
+//     zeroline: false
+//   },
+//   yaxis2: {
+//     domain: [0.85, 1],
+//     showgrid: false,
+//     zeroline: false
+//   }
+// };
+// // console.log(x);
+// // console.log(y);
+// // Plotly.newPlot('2D_Contour', data, layout);
+
+
+// function createHeatPlot(folder_path, file_event, event_name, div_name) {
+//     var data = [];
+
+
+//     function createTrace(data, filename, player, mode){
+
+//         d3.csv(filename, function(err, rows) {
+        
+//             function unpack(rows, key) {
+//                 return rows.map(function(row)
+//                 { return row[key]; });
+//             }
+    
+//             r = Math.floor(Math.random() * 256).toString()
+//             g = Math.floor(Math.random() * 256).toString()
+//             b = Math.floor(Math.random() * 256).toString()
+//             random_color = 'rgb(' + r + ',' + g + ',' + b +')';
+
+//             var trace1 = {
+//                 x: unpack(rows, 'x'),
+//                 y: unpack(rows, 'z'),
+//                 mode: mode,
+//                 name: player,
+//                 marker: {
+//                   color: random_color,
+//                   size: 3,
+//                 },
+//                 type: 'scatter'
+//               };
+//               var trace2 = {
+//                 x: unpack(rows, 'x'),
+//                 y: unpack(rows, 'z'),
+//                 name: 'density',
+//                 // ncontours: 100,
+//                 colorscale: 'Hot',
+//                 reversescale: true,
+//                 showscale: false,
+//                 type: 'histogram2dcontour'
+//               };
+//               var trace3 = {
+//                 x: unpack(rows, 'x'),
+//                 name: player + ' x density',
+//                 marker: {color: random_color},
+//                 yaxis: 'y2',
+//                 type: 'histogram'
+//               };
+//               var trace4 = {
+//                 y: unpack(rows, 'z'),
+//                 name: 'y density',
+//                 marker: {color: random_color},
+//                 xaxis: 'x2',
+//                 type: 'histogram'
+//               };
+//             data.push(trace1, trace2, trace3, trace4);
+//         });
+//     }
+
+
+//     for(var i = 0; i < 40; i++){
+//         createTrace(data, folder_path + i.toString() + file_event + '.csv', 'Player ' + i.toString(), 'markers');
+
+//         // console.log(x_arr)
+//     }
+    
+//     var layout = {
+//         title: event_name,
+//         showlegend: true,
+//         autosize: false,
+//         width: 1500,
+//         height: 1500,
+//         margin: {t: 50},
+//         hovermode: 'closest',
+//         bargap: 0,
+//         xaxis: {
+//           showgrid: false,
+//           zeroline: false
+//         },
+//         yaxis: {
+//           showgrid: false,
+//           zeroline: false
+//         },
+//         xaxis2: {
+//           domain: [0.95, 1],
+//           showgrid: false,
+//           zeroline: false
+//         },
+//         yaxis2: {
+//           domain: [0.95, 1],
+//           showgrid: false,
+//           zeroline: false
+//         }
+//       };
+    
+//     setTimeout(function(){ console.log(data); Plotly.newPlot(div_name, data, layout, {responsive: true, displaylogo: false}); }, 5000);
+// }
+
+// createHeatPlot('./data/end/', '_move', 'PlayerMoveEvent', 'End_Move_2DHeat');
--- src/js/3D.js
+++ src/js/3D.js
@@ -1,245 +1,79 @@
-var data = [];
+function createPlot(folder_path, file_event, event_name, div_name, random_color) {
+    var data = [];
 
-function createTrace(filename, player, mode, type){
+    function createTrace(data, filename, player, mode, type, random_color){
 
-    var ret;
-    d3.csv(filename, function(err, rows) {
-    
-        function unpack(rows, key) {
-            return rows.map(function(row)
-            { return row[key]; });
-        }
+        d3.csv(filename, function(err, rows) {
+        
+            function unpack(rows, key) {
+                return rows.map(function(row)
+                { return row[key]; });
+            }
 
-        r = Math.floor(Math.random() * 256).toString()
-        g = Math.floor(Math.random() * 256).toString()
-        b = Math.floor(Math.random() * 256).toString()
-        random_color = 'rgb(' + r + ',' + g + ',' + b +')';
-        var trace = {
-        x: unpack(rows, 'x'),
-        y: unpack(rows, 'z'),
-        z: unpack(rows, 'y'),
-        mode: mode,
-        name: player,
-        marker: {
-            color: random_color,
-            size: 3,
-            symbol: 'circle',
+            var trace = {
+            x: unpack(rows, 'x'),
+            y: unpack(rows, 'z'),
+            z: unpack(rows, 'y'),
+            mode: mode,
+            name: player,
+            marker: {
+                color: random_color,
+                size: 3,
+                symbol: 'circle',
+                line: {
+                    color: random_color,
+                    width: 0
+                }},
             line: {
                 color: random_color,
-                width: 0
-            }},
-        line: {
-            color: random_color,
-            width: 1
-        },
-        type: type
-        };
-        data.push(trace);
-        console.log(data.length)
-        
-    });
-
-    
-}
-for(var i = 0; i < 40; i++){
-    createTrace('./data/'+ i.toString() + '_move.csv', 'Player ' + i.toString(), 'lines+markers', 'scatter3d')
-}
-
-console.log(data[1])
-
-var layout = {
-    title: 'ALL PLAYER PLOT',
-    autosize: true,
-    width: 1500,
-    height: 1500,
-    margin: {
-        l: 0,
-        r: 0,
-        b: 0,
-        t: 65
+                width: 1
+            },
+            type: type
+            };
+            data.push(trace);
+        });
     }
-};
 
-setTimeout(function(){ console.log(data); Plotly.newPlot('my3DPoints', data, layout, {responsive: true, displaylogo: false}); }, 5000);
+    for(var i = 0; i < 40; i++){
+        createTrace(data, folder_path + i.toString() + file_event + '.csv', 'Player ' + i.toString(), 'markers', 'scatter3d', random_color[i])
+    }
 
+    var layout = {
+        title: event_name,
+        autosize: true,
+        width: 1500,
+        height: 1500,
+        margin: {
+            l: 0,
+            r: 0,
+            b: 0,
+            t: 65
+        }
+    };
 
-// var data1 = [];
-// function createHeat(filename, player, mode, type){
+    setTimeout(function(){ console.log(data); Plotly.newPlot(div_name, data, layout, {responsive: true, displaylogo: false}); }, 5000);
+}
 
-//     var ret;
-//     d3.csv(filename, function(err, rows) {
-    
-//         function unpack(rows, key) {
-//             return rows.map(function(row)
-//             { return row[key]; });
-//         }
+// set the same random color per player, instead of per plot
+var random_color = [];
+for (i = 0; i < 40; i++) {
+    r = Math.floor(Math.random() * 256).toString()
+    g = Math.floor(Math.random() * 256).toString()
+    b = Math.floor(Math.random() * 256).toString()
+    random_color[i] = 'rgb(' + r + ',' + g + ',' + b +')';
+}
 
-//         r = Math.floor(Math.random() * 256).toString()
-//         g = Math.floor(Math.random() * 256).toString()
-//         b = Math.floor(Math.random() * 256).toString()
-//         random_color = 'rgb(' + r + ',' + g + ',' + b +')';
-//         var trace = {
-//         x: unpack(rows, 'x'),
-//         y: unpack(rows, 'z'),
-//         z: unpack(rows, 'y'),
-//         mode: mode,
-//         name: player,
-//         marker: {
-//             color: random_color,
-//             // colorscale: 'Hot',
-//             size: 1,
-//             symbol: 'circle',
-//             line: {
-//                 color: random_color,
-//                 width: 0
-//             }},
-//         line: {
-//             // color: random_color,
-//             width: 1
-//         },
-//         type: type
-//         };
-//         data1.push(trace);
-//         console.log(data1.length)
-        
-//     });
+createPlot('./data/overworld/move/', '_move', 'PlayerMoveEvent', 'Overworld_Move_3DPoints', random_color);
+createPlot('./data/overworld/death/', '_death', 'PlayerDeathEvent', 'Overworld_Death_3DPoints', random_color);
+createPlot('./data/overworld/block_break/', '_block_break', 'BlockBreakEvent', 'Overworld_BlockBreak_3DPoints', random_color);
+createPlot('./data/overworld/block_place/', '_block_place', 'BlockPlaceEvent', 'Overworld_BlockPlace_3DPoints', random_color);
 
-    
-// }
-// for(var i = 0; i < 40; i++){
-//     createHeat('./data/'+ i.toString() + '_move.csv', 'Player ' + i.toString(), 'markers', 'scatter3d')
-// }
+createPlot('./data/nether/move/', '_move', 'PlayerMoveEvent', 'Nether_Move_3DPoints', random_color);
+createPlot('./data/nether/death/', '_death', 'PlayerDeathEvent', 'Nether_Death_3DPoints', random_color);
+createPlot('./data/nether/block_break/', '_block_break', 'BlockBreakEvent', 'Nether_BlockBreak_3DPoints', random_color);
+createPlot('./data/nether/block_place/', '_block_place', 'BlockPlaceEvent', 'Nether_BlockPlace_3DPoints', random_color);
 
-// console.log(data1[1])
-
-// var layout = {
-//     title: 'ALL PLAYER PLOT',
-//     autosize: true,
-//     width: 1500,
-//     height: 1500,
-//     // showscale:true,
-//     // colorscale: 'Hot',
-//     margin: {
-//         l: 0,
-//         r: 0,
-//         b: 0,
-//         t: 65
-//     }
-// };
-
-// setTimeout(function(){ console.log(data1); Plotly.newPlot('my3DHeat', data1, layout, {responsive: true, displaylogo: false}); }, 5000);
-
-
-
-// var data = $.csv.toObjects("../../data/epilog_data.csv");
-
-// $(document).ready(function() {
-//     $.ajax({
-//         type: "GET",
-//         url: "http://127.0.0.1:5500/data/epilog_data.txt",
-//         dataType: "text",
-//         success: function(data) {processData(data);}
-//      });
-// });
-
-
-
-// function processData(allText) {
-//     var allTextLines = allText.split(/\r\n|\n/);
-//     var headers = allTextLines[0].split(',');
-//     var lines = [];
-
-//     for (var i=1; i<allTextLines.length; i++) {
-//         var data = allTextLines[i].split(',');
-//         if (data.length == headers.length) {
-
-//             var tarr = [];
-//             for (var j=0; j<headers.length; j++) {
-//                 tarr.push(headers[j]+":"+data[j]);
-//             }
-//             lines.push(tarr);
-//         }
-//     }
-//     // alert(lines);
-//     console.log(lines);
-// }
-
-// var trace1 = {
-//     x: data.x1,
-//     y: data.y1,
-//     z: data.z1,
-//     mode: 'markers',
-//     name: "Player 1",
-//     marker: {
-//         color: 'rgb(255,20,147)',
-//         size: 2,
-//         symbol: 'circle',
-//         line: {
-//             color: 'rgb(255,20,147)',
-//             width: 0
-//         }},
-//     line: {
-//         color: '#1f77b4',
-//         width: 2
-//     },
-//     type: 'scatter3d'
-// };
-        
-            // var trace2 = {
-            // x: unpack(rows, 'x2'),
-            // y: unpack(rows, 'y2'),
-            // z: unpack(rows, 'z2'),
-            // mode: 'markers',
-            // name: "Player 2",
-            // marker: {
-            //     color: '#9467bd',
-            //     size: 2,
-            //     symbol: 'circle',
-            //     line: {
-            //     color: 'rgb(0,0,0)',
-            //     width: 0
-            //     }},
-            // line: {
-            //     color: 'rgb(44, 160, 44)',
-            //     width: 1
-            // },
-            // type: 'scatter3d'
-            // };
-        
-            // var trace3 = {
-            // x: unpack(rows, 'x3'),
-            // y: unpack(rows, 'y3'),
-            // z: unpack(rows, 'z3'),
-            // mode: 'markers',
-            // name: "Player 3",
-            // marker: {
-            //     color: '#bcbd22',
-            //     size: 2,
-            //     symbol: 'circle',
-            //     line: {
-            //     color: 'rgb(0,0,0)',
-            //     width: 0
-            //     }},
-            // line: {
-            //     color: '#bcbd22',
-            //     width: 1
-            // },
-            // type: 'scatter3d'
-            // };
-        
-// var data = [trace1];
-// var layout = {
-//     title: '3D Line Plot',
-//     autosize: true,
-//     width: 1000,
-//     height: 1000,
-//     margin: {
-//         l: 0,
-//         r: 0,
-//         b: 0,
-//         t: 65
-//     }
-// };
-
-// Plotly.newPlot('my3DLine2', data, layout, {responsive: true, displaylogo: false});
-
+createPlot('./data/end/move/', '_move', 'PlayerMoveEvent', 'End_Move_3DPoints', random_color);
+createPlot('./data/end/death/', '_death', 'PlayerDeathEvent', 'End_Death_3DPoints', random_color);
+createPlot('./data/end/block_break/', '_block_break', 'BlockBreakEvent', 'End_BlockBreak_3DPoints', random_color);
+createPlot('./data/end/block_place/', '_block_place', 'BlockPlaceEvent', 'End_BlockPlace_3DPoints', random_color);
