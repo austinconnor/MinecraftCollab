0b2b202924cdd84215b895b977f9704b3dbe8c92
Chloe Geller <chloegeller@knigths.ucf.edu>
2021-11-04 10:59:20 -0400

    started 3D heatmap for density

--- src/js/3D.js
+++ src/js/3D.js
@@ -28,7 +28,7 @@ function createPlot(folder_path, file_event, event_name, div_name, random_color)
                 color: random_color,
                 width: 1
             },
-            type: type
+            type: type,
             };
             data.push(trace);
         });
@@ -77,3 +77,70 @@ createPlot('./data/end/move/', '_move', 'PlayerMoveEvent', 'End_Move_3DPoints', 
 createPlot('./data/end/death/', '_death', 'PlayerDeathEvent', 'End_Death_3DPoints', random_color);
 createPlot('./data/end/block_break/', '_block_break', 'BlockBreakEvent', 'End_BlockBreak_3DPoints', random_color);
 createPlot('./data/end/block_place/', '_block_place', 'BlockPlaceEvent', 'End_BlockPlace_3DPoints', random_color);
+
+
+// function createHeatPlot(folder_path, file_event, event_name, div_name) {
+//     var data = [];
+
+//     function createHeatTrace(data, filename, player, mode, type){
+
+//         d3.csv(filename, function(err, rows) {
+        
+//             function unpack(rows, key) {
+//                 return rows.map(function(row)
+//                 { return row[key]; });
+//             }
+
+//             var trace = {
+//             x: unpack(rows, 'x'),
+//             y: unpack(rows, 'z'),
+//             z: unpack(rows, 'y'),
+//             mode: mode,
+//             name: player,
+//             marker: {
+//                 color: unpack(rows, 'time'),
+//                 size: 2,
+//                 cauto:true,
+//                 symbol: 'circle',
+//                 colorscale: 'Hot',
+//                 reversescale: false,
+//                 line: {
+//                     // color: random_color,
+//                     width: 0
+//                 }},
+//             line: {
+//                 // color: random_color,
+//                 width: 1
+//             },
+//             type: type,
+//             colorscale: 'Hot',
+//             showscale: true,
+//             reversescale: false,
+//             };
+//             data.push(trace);
+//         });
+//     }
+
+//     for(var i = 0; i < 40; i++){
+//         createHeatTrace(data, folder_path + i.toString() + file_event + '.csv', 'Player ' + i.toString(), 'markers', 'scatter3d')
+//     }
+
+//     var layout = {
+//         title: event_name,
+//         showscale: true,
+//         reversescale: true,
+//         autosize: true,
+//         width: 1500,
+//         height: 1500,
+//         margin: {
+//             l: 0,
+//             r: 0,
+//             b: 0,
+//             t: 65
+//         }
+//     };
+
+//     setTimeout(function(){ console.log(data); Plotly.newPlot(div_name, data, layout, {responsive: true, displaylogo: false}); }, 5000);
+// }
+
+// createHeatPlot('./data/overworld/move/', '_move', 'PlayerMoveEvent', 'Overworld_Move_3DHeat');
